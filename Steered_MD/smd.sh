4e828c292b7248a98b9ac7f1c09867e61fb865be
u16chy01 <47016067+u16chy01@users.noreply.github.com>
2021-01-20 11:04:30 +0800

    Add files via upload

---
+++ Steered_MD/smd.sh
@@ -0,0 +1,2 @@
+pmemd.cuda -O -i md_NVT_S1.in -o tyr-src-wat-eq3ns-s1.out -p tyr-src-wat.prmtop -c tyr-src-wat-eq3ns-30.rst -r tyr-src-wat-eq3ns-s1.rst -ref tyr-src-wat-eq3ns-30.rst -x tyr-src-wat-eq3ns-s1.mdcrd
+
